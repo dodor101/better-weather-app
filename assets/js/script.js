f3079f00544d20bde84912bddf35a7a16e1360e8
Dukenson Dodor <104813218+dodor101@users.noreply.github.com>
2023-09-15 09:15:43 -0500

    Add Complete styling and layout with bootstrap and JS functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,41 @@
-console.log('Hello World');
+// console.log('Hello World');
+// let date = dayjs().format('DD/MM/YYYY');
+// console.log(date);
+const cityFormEl = $('#form-city');
+const searchHistoryBtn = $('#display-search-history');
+const cityInput = $('#city-name');
+
+const APIKey = '3911b3545970347c6b98f6c645c609f0';
+async function getWeatherInfo(city) {
+  // only accept type String
+  if (typeof city !== 'string') {
+    alert('You must use Letters only!');
+    return;
+  }
+  // declare  queryURL
+  const queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
+
+  // fetch data with ajax
+  const getData = await $.ajax({
+    url: queryURL,
+    method: 'GET',
+  });
+
+  return getData;
+}
+
+async function getCity() {
+  let city = cityInput.val();
+  let weatherData = await getWeatherInfo(city);
+  printData(weatherData);
+}
+cityFormEl.on('submit', (event) => {
+  event.preventDefault();
+  getCity();
+});
+
+// write a function that print
+
+function printData(data) {
+  console.log(data);
+}
